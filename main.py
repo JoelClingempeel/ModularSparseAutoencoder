816e046824ba76b864d6a708afeab4972c2387d2
Joel Clingempeel <joel.clingempeel@gmail.com>
2020-12-04 21:08:03 -0500

    Removed flag-guarding for logging flags since without this flag on the analyze colab crashes.

--- main.py
+++ main.py
@@ -19,10 +19,10 @@ parser.add_argument('--intermediate_dim', type=int, default=250)
 parser.add_argument('--stripe_dim', type=int, default=5)
 parser.add_argument('--num_stripes', type=int, default=30)
 parser.add_argument('--num_active_neurons', type=int, default=15)
-parser.add_argument('--num_active_stripes', type=int, default=5)
+parser.add_argument('--num_active_stripes', type=int, default=3)
 parser.add_argument('--layer_sparsity_mode', type=str, default='none')  # Set to none, ordinary, boosted, or lifetime.
 parser.add_argument('--stripe_sparsity_mode', type=str, default='routing')  # Set to none, ordinary, or routing.
-parser.add_argument('--distort_prob', type=float, default=.1)  # Probability of stripe sparsity mask bits randomly flipping.
+parser.add_argument('--distort_prob', type=float, default=.4)  # Probability of stripe sparsity mask bits randomly flipping.
 parser.add_argument('--distort_prob_decay', type=float, default=.025)  # Lowers distort_prob by this amount every epoch.
 
 # Boosting Flags - Only necessary when layer_sparsity_mode is set to boosted.
@@ -40,13 +40,12 @@ parser.add_argument('--active_stripes_per_batch', type=float, default=1.)
 # Training Flags
 parser.add_argument('--lr', type=float, default=.01)
 parser.add_argument('--momentum', type=float, default=.9)
-parser.add_argument('--num_epochs', type=int, default=2)
+parser.add_argument('--num_epochs', type=int, default=20)
 parser.add_argument('--batch_size', type=int, default=8)
 parser.add_argument('--data_path', type=str, default='data.csv')
 parser.add_argument('--log_path', type=str, default='logs')
 parser.add_argument('--log_class_specific_losses', type=bool, default=False)
 parser.add_argument('--log_average_activations', type=bool, default=True)
-parser.add_argument('--log_experiment_flags', type=bool, default=True)
 parser.add_argument('--use_cuda_if_available', type=bool, default=True)
 
 args = vars(parser.parse_args())
@@ -116,10 +115,9 @@ def main(args):
             average_activations = net.get_average_activations(X_test, Y_test, device=device).tolist()
             f.write(json.dumps(average_activations))
 
-    if args['log_experiment_flags']:
-        flag_values_path = os.path.join(root_path, 'experiment_config.json')
-        with open(flag_values_path, 'w') as f:
-            f.write(json.dumps(args))
+    flag_values_path = os.path.join(root_path, 'experiment_config.json')
+    with open(flag_values_path, 'w') as f:
+        f.write(json.dumps(args))
 
 if __name__ == '__main__':
     main(args)
